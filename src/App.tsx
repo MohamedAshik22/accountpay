7bb870605a258469cf919cb84bd1fb9d0af4bea4
MohamedAshik22 <ashikmd256@gmail.com>
2025-05-03 21:55:24 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     
+       <div className='text-2xl font-extrabold text-red-300'> HEllo</div>
+    </>
+  )
+}
+
+export default App
